78b63a8cc36e3c28ed9d7ff701eb24a9125e0c4c
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-03 22:18:39 -0500

    Move logic over to fragment

---
+++ app/src/main/java/com/example/timetracking/host/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.timetracking.host
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.timetracking.R
+import com.example.timetracking.login.LoginFragment
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            val fragment = LoginFragment()
+            supportFragmentManager.beginTransaction()
+                .add(fragment, fragment.tag)
+                .commit()
+        }
+    }
+}
