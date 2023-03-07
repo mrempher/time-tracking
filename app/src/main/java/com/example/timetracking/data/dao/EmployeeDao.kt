d49b56127b8ff76939b50ea1003e01f5eac7aa67
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 08:13:12 -0500

    add conflict resolution for insert

--- app/src/main/java/com/example/timetracking/data/dao/EmployeeDao.kt
+++ app/src/main/java/com/example/timetracking/data/dao/EmployeeDao.kt
@@ -2,6 +2,7 @@ package com.example.timetracking.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.timetracking.data.entity.Employee
 import kotlinx.coroutines.flow.Flow
@@ -14,9 +15,9 @@ interface EmployeeDao {
     @Query("SELECT * FROM employee WHERE id = :id")
     fun getEmployee(id: String): Flow<Employee>
 
-    @Query("SELECT * FROM employee WHERE loginName = :userName")
+    @Query("SELECT * FROM employee WHERE login_name = :userName")
     fun getEmployeeByUserName(userName: String): Flow<Employee>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEmployees(employee: List<Employee>)
 }
