52384eca33461dd4c6b771c7fccfbf4bcd6bea48
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 16:12:52 -0500

    setup Toolbar

--- app/src/main/java/com/example/timetracking/timekeeping/TimeTrackingFragment.kt
+++ app/src/main/java/com/example/timetracking/timekeeping/TimeTrackingFragment.kt
@@ -3,9 +3,7 @@ package com.example.timetracking.timekeeping
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.timetracking.R
@@ -14,7 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 const val TAG = "TimeTrackingFragment"
 @AndroidEntryPoint
-class TimeTrackingFragment : Fragment(), MenuProvider {
+class TimeTrackingFragment : Fragment() {
     private lateinit var binding: FragmentTimeTrackingBinding
 
     private val viewModel by viewModels<TimeTrackingViewModel>()
@@ -26,30 +24,22 @@ class TimeTrackingFragment : Fragment(), MenuProvider {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = FragmentTimeTrackingBinding.inflate(inflater, container, false)
+
         args.employeeArgument?.let {
             viewModel.setEmployeeData(it)
         }
 
-        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
-
-        binding = FragmentTimeTrackingBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.menu_main, menu)
-    }
-
-    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        return when (menuItem.itemId) {
-            R.id.nav_logout -> {
-                findNavController().popBackStack(R.id.loginFragment, false)
-                true
-            }
-            else -> {
-                false
+        binding.toolbar.inflateMenu(R.menu.menu_main)
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.nav_logout -> {
+                    findNavController().popBackStack(R.id.loginFragment, false)
+                    true
+                }
+                else -> { false }
             }
         }
+        return binding.root
     }
-
 }
