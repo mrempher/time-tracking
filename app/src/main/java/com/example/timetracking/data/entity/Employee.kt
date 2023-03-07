b5950a4f63727aa507d026e3f6d0d92692597868
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 08:12:58 -0500

    add columnInfo annotation

--- app/src/main/java/com/example/timetracking/data/entity/Employee.kt
+++ app/src/main/java/com/example/timetracking/data/entity/Employee.kt
@@ -1,14 +1,14 @@
 package com.example.timetracking.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "employee")
+@Entity
 data class Employee(
-    @PrimaryKey(autoGenerate = true)
-    var id: Int? = null,
-    var isAdmin: Int,
-    var loginName: String,
-    var loginPassword: String,
-    var employeeName: String
+    @PrimaryKey(autoGenerate = true) var id: Int? = null,
+    @ColumnInfo(name = "is_admin") var isAdmin: Int,
+    @ColumnInfo(name = "login_name") var loginName: String,
+    @ColumnInfo(name = "login_password") var loginPassword: String,
+    @ColumnInfo(name = "employee_name") var employeeName: String
 )
