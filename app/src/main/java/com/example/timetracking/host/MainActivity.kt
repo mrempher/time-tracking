0ccc5f995bb0f125f32c65cb89d65ca34ec33508
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 08:07:27 -0500

    add entryPoint for Hilt

--- app/src/main/java/com/example/timetracking/host/MainActivity.kt
+++ app/src/main/java/com/example/timetracking/host/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.timetracking.R
 import com.example.timetracking.login.LoginFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+const val TAG = "MainActivity"
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
