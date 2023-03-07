a29757328982a17f427de8170e314c2c1feea3ae
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 15:15:59 -0500

    make Employee Parcelable

--- app/src/main/java/com/example/timetracking/data/entity/Employee.kt
+++ app/src/main/java/com/example/timetracking/data/entity/Employee.kt
@@ -1,9 +1,12 @@
 package com.example.timetracking.data.entity
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class Employee(
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
@@ -11,4 +14,6 @@ data class Employee(
     @ColumnInfo(name = "login_name") var loginName: String = "",
     @ColumnInfo(name = "login_password") var loginPassword: String = "",
     @ColumnInfo(name = "employee_name") var employeeName: String = ""
-)
+): Parcelable {
+
+}
