d1bdddd718c4a3b24120c6d4671b87557354ca6b
Michael Rempher <Michael.Rempher@walmart.com>
2023-03-07 17:04:59 -0500

    add new fab logic for showing/hiding

--- app/src/main/java/com/example/timetracking/timekeeping/TimeTrackingFragment.kt
+++ app/src/main/java/com/example/timetracking/timekeeping/TimeTrackingFragment.kt
@@ -1,7 +1,9 @@
 package com.example.timetracking.timekeeping
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -17,6 +19,8 @@ class TimeTrackingFragment : Fragment() {
 
     private val viewModel by viewModels<TimeTrackingViewModel>()
     private val args: TimeTrackingFragmentArgs by navArgs()
+
+    private var isAllFabsVisible: Boolean = false
 
 
     override fun onCreateView(
@@ -40,6 +44,35 @@ class TimeTrackingFragment : Fragment() {
                 else -> { false }
             }
         }
+
+        binding.timeFab.shrink()
+
+        binding.timeFab.setOnClickListener { handleFabVisibility() }
+
+
+
+
         return binding.root
+    }
+
+    private fun handleFabVisibility() {
+        if (!isAllFabsVisible) {
+            binding.startTimeFab.visibility = View.VISIBLE
+            binding.startTimeFabText.visibility = View.VISIBLE
+            binding.endTimeFab.visibility = View.VISIBLE
+            binding.endTimeFabText.visibility = View.VISIBLE
+
+            binding.timeFab.extend()
+
+            isAllFabsVisible = true
+        } else {
+            binding.startTimeFab.visibility = View.GONE
+            binding.startTimeFabText.visibility = View.GONE
+            binding.endTimeFab.visibility = View.GONE
+            binding.endTimeFabText.visibility = View.GONE
+
+            binding.timeFab.shrink()
+            isAllFabsVisible = false
+        }
     }
 }
